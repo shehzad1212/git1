c6af8265beb1a0ab1789dbdc85f111097fca7380
shehzad1212 <tawhidulislam261@gmail.com>
2021-05-02 17:01:52 +0600

    first commit

---
+++ res/js/script.js
@@ -0,0 +1,26 @@
+var mixer = mixitup(".grid-portfolio-container");
+
+$(function () {
+  $.scrollify({
+    section: ".scrollify",
+    setHeights: false,
+    updateHash: false,
+  });
+});
+function openMenu() {
+  document.getElementById("navbar").style.height = "100%";
+}
+function closeMenu() {
+  document.getElementById("navbar").style.height = "0%";
+}
+$(function () {
+  $(".menu-item a .scroll-down a").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: $($.attr(this, "href")).offset().top,
+      },
+      1000
+    );
+    return false;
+  });
+});
